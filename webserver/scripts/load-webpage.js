0dbfed968805f289b2894b4682d52a5ed0723e11
Alejandro Servetto <aservet1@binghamton.edu>
2022-03-26 13:40:38 -0400

    more frontend features: book options, help message, and available books in a given book. also started to work on a "process text into html table", analogous to the current "process text into html list", but thats mostly just a stub function right now

--- webserver/scripts/load-webpage.js
+++ webserver/scripts/load-webpage.js
@@ -1,9 +1,10 @@
 function httpGetAsync(theUrl, callback)
 { // thanks to https://stackoverflow.com/questions/247483/http-get-request-in-javascript
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            callback(xmlHttp.responseText)
+        }
     }
     xmlHttp.open("GET", theUrl, true); // true for asynchronous 
     xmlHttp.send(null);
@@ -24,6 +25,30 @@ function parseBookText(bookText) {
             return parsedBook
         }, {}
     )
+}
+function postProcessBibleTextToHTMLTable__NOTREADYTOUSEYET(text) {
+    // this function is not done, I've only copied the code from postProcessBibleTextToHTMLList and started to make a few small changes
+    let str = '';
+    str += '<table>'
+    for (const [book, chapters] of Object.entries(parseBookText(text))) {
+        str += '<li><b>' + book + '</b>'
+        str += '<ul>'
+        for (const [chapter,verses] of Object.entries(chapters)) {
+            str += '<li><b>Chapter ' + chapter + ':</b>'
+            str += '<ul>'
+            for (const [verse,verseText] of Object.entries(verses)) {
+                str += '<li>'
+                str += '<b>'+verse+'.</b> ' + verseText
+                str += '</li>'
+            }
+            str += '</ul>'
+            str += '</li>'
+        }
+        str += '</ul>'
+        str += '</li>'
+    }
+    str += '</table>'
+    return str // text
 }
 function postProcessBibleTextToHTMLList(text) {
     let str = '';
@@ -46,10 +71,14 @@ function postProcessBibleTextToHTMLList(text) {
         str += '</li>'
     }
     str += '</ul>'
-    return str // text
+    return str
 }
 function showBibleText(text) {
     document.getElementById('contentSpot').innerHTML = postProcessBibleTextToHTMLList(text)
+}
+function showText(text) {
+    // console.log(text)
+    document.getElementById('contentSpot').innerText = text
 }
 function reverseString(str) {
 	let rev = ""
@@ -57,10 +86,53 @@ function reverseString(str) {
 		rev += str[i]
 	} return rev;
 }
-function getCmd(cmd) {
+function getBookSelectionName() {
+    const radioButtons = document.querySelectorAll('input[name="book_selection"]');
+    let selection
+    for (const radioButton of radioButtons) {
+        if (radioButton.checked) {
+            selection = radioButton.value
+            break
+        }
+    }
+    return selection
+}
+function getFullURLFromCmd(cmd) {
+    return "http://149.28.52.2:8080/api/booktext?cmd=" + (
+        getBookSelectionName() + ' ' + cmd
+    ).split(/\s+/).join('_')
+}
+function getBookText(bookrequest) {
     httpGetAsync(
-        "http://149.28.52.2:8080/api/booktext?cmd=" + cmd.split(/\s+/).join('_'),
+        getFullURLFromCmd(bookrequest),
         showBibleText
     )
 }
-getCmd('kjv mark 16:1,2,4,8')
+function validInput(text) {
+
+}
+document.getElementById('askbutton').onclick = (
+    function() {
+        let text = document.getElementById('asktext').value
+        // console.log(text)
+        // TODO: evalate text input for validity, 
+        getBookText(text)
+    }
+)
+document.getElementById('askforhelp').onclick = (
+    function() {
+        console.log('hello!')
+        httpGetAsync(
+            getFullURLFromCmd(getBookSelectionName()+' -help'),
+            showText
+        )
+    }
+)
+document.getElementById('askforbooks').onclick = (
+    function() {
+        httpGetAsync(
+            getFullURLFromCmd(getBookSelectionName()+' -l'),
+            showText
+        )
+    }
+)
