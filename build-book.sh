0f2b7458b7ef85cdf0e5e3bbef0bc7bbeab84777
Alejandro Servetto <aservet1@binghamton.edu>
2022-06-05 22:09:43 -0400

    verbose running of build book script

--- build-book.sh
+++ build-book.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
+set -v
 bookfile=$1
 awk -f ./data/generate.awk $bookfile > data/bible_data.c
 exe=$( echo $bookfile | sed 's|^.*/||;s|\..*$||' )
 make
 mkdir -p bin
 mv bible bin/$exe
-
