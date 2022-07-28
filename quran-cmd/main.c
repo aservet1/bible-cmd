c6e3c3a35c2846f90df47d23d8b6f09af65b3801
Alejandro Servetto <aservet1@binghamton.edu>
2022-07-28 03:07:26 -0400

    misc progress, i left this a while ago so i dont remember exactly everything. but saving it so I dont lose it

---
+++ quran-cmd/main.c
@@ -0,0 +1,5 @@
+#include "quran_data.h"
+// extern int surah_count = 114;
+// extern char* quran[][114];
+
+
---
+++ quran-cmd/quran.h
@@ -0,0 +1,5 @@
+#pragma once
+
+extern int surah_count = 114;
+extern char* quran[][114];
+
